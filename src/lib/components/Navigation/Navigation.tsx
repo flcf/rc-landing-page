bed2c52e54b5cff4ec4fefa66f6832002d6810e4
flcf <tcbnar@gmail.com>
2024-12-08 22:13:54 -0800

    setting up routing

--- src/lib/components/Navigation/Navigation.tsx
+++ src/lib/components/Navigation/Navigation.tsx
@@ -1,10 +1,14 @@
 const Navigation: React.FC = () => {
   return (
-    <nav>
-      <a className="hidden" href="/">
-        <img src="/assets/Logo.png" alt="Logo" />
+    <nav className="flex items-center justify-between h-16 w-full px-[15vw] mx-auto overflow-hidden">
+      <a href="/">
+        <img
+          src="/assets/Logo.png"
+          alt="Logo"
+          className="scale-[1.2] max-w-[40vw]"
+        />
       </a>
-      <a className="hidden" href="/about">
+      <a href="/about" className="font-montserrat font-medium">
         About Us
       </a>
     </nav>
@@ -12,4 +16,3 @@ const Navigation: React.FC = () => {
 };
 
 export default Navigation;
- 
