39787a08ddf486f7c28a5f522acd320c78e4d711
R Lee <alpha.r.lee.dev@gmail.com>
2026-02-22 16:35:03 -0700

    Refactor to ChevronButton

--- src/lib/components/Hero/Hero.tsx
+++ src/lib/components/Hero/Hero.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ChevronButton from "../ChevronButton/ChevronButton";
 const Hero: React.FC = () => {
   return (
     <section
@@ -51,33 +52,10 @@ const Hero: React.FC = () => {
           Join us in celebrating Asian pop culture, empowering local
           communities, and supporting minority-owned businesses.
         </p>
-        <button
-          className="
-            btn mt-4
-            font-montserrat
-            text-base sm:text-lg md:text-xl
-            px-6 py-2
-            flex items-center gap-2 shadow-lg
-        "
-        >
-          Join Our Next Event
-          {/* Chevron SVG */}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={3}
-            stroke="white"
-            className="w-5 h-5 ml-2"
-            aria-hidden="true"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M9 5l7 7-7 7"
-            />
-          </svg>
-        </button>
+        <ChevronButton 
+          onClick={() => {}}
+          label='Join Our Next Event'
+        />
       </div>
       {/* SVGs - adjust position and size responsively */}
       <svg
